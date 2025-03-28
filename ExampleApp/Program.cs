b203f24da6c453da655cb846dc38102879f3fad3
Singh <2401678@cognizant.com>
2025-03-28 12:20:40 +0530

    first commit

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Print "Hello, World!"
+        Console.WriteLine("Hello, World!");
+
+        // Ask for the user's name
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        // Greet the user
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
